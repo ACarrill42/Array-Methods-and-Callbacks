e262448e3393b61dcd56729a200fc646600c5841
ACarrill42 <austincarrill@gmail.com>
2020-10-08 11:11:37 -0500

    solved task 4

--- index.js
+++ index.js
@@ -54,15 +54,19 @@ const getYears = getFinals.map((item) => {
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(winners) {
-    
-    let winners = [];
-    let getAllWinners = winners.map
-    
+function getWinners(winner) {
+    let winners = winner.map((game) => {
+      if(game["Home Team Goals"] > game["Away Team Goals"]) {
+        return game["Home Team Name"];
+      }else {
+        return game["Away Team Name"];
+      };
+    });
+    return winner;
 
 };
 
-console.log(getWinners(getFinals);
+console.log(getWinners(getFinals(fifaData)));
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
